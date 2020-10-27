982df89f3c3322ba85b872d1841dbf40ac9bdb2e
epic0448 <samuelogundiran@Yahoo.com>
2020-10-27 12:59:35 -0400

    Problems are solved

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+//https://www.youtube.com/watch?v=rRgD1yVwIvE
+
 const companies = [
     {name: "Company One", category: "Finance", start: 1981, end: 2003},
     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
@@ -19,22 +21,55 @@ const companies = [
     
   // }
 
-  //forEach
 
+// ---------    ForEach
+
+// companies.forEach(function (company, index, companies) {
+//   // Can take up to 3 Argment.  
     
-companies.forEach(function (company, index, companies) {
-  console.log(company.name, company.start, "-", company.end)
-    // => Company One 1981 - 2003  . . . For each company 
-})
+//   console.log(company.name,  company.start, "-", company.end, index )
+//     /*  => Company Two 1992 - 2008  1   . . .   For each company in our array 
+//            Company three 1999 - 2007  2   . . . 
+//      */
+// })
       
-    ;
+let age = [33, 12, 20, 16,100, 5, 54, 21, 44, 61];
+
+    // --------filter----- GET 21 AN OLDER
+
+//      // Using ForLoop
+// let canDrink = []
+// for (let i = 0; i < age.length; i++){
+//   if (age[i] >= 15) {
+  
+//     canDrink.push(age[i])
+//   } 
+// }
+// console.log(canDrink)   //=> [ 33, 20, 16, 54, 21, 44, 61 ]
+
+// const canDrink = age.filter(function (eachAge) {
+//    // If U want in order. We use SORT. 
+//   if (eachAge >= 21) return age.sort(function(a,b){return a - b});
+//  })
+// console.log(canDrink) 
+ 
+
+  // Filter out companies that are Retails. 
+
+const retailComp = companies.filter(function (company) {
+  // console.log(company.category)
+   if(company.category === "Retail") return company
+})
+   console.log(retailComp)
+
+//............ End of Filter........
 
   //map
   //sort
   //reduce
   //for..in
   //spread
-  //filter 
+  
 
   //splice..
   // ..
