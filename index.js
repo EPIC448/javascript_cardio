a4b4e75b9bc6d3403ea60cc57f1d608e2ca7fff6
epic0448 <samuelogundiran@Yahoo.com>
2020-10-27 16:07:16 -0400

    JavaScript Higher order function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 //https://www.youtube.com/watch?v=rRgD1yVwIvE
 
+// NOte.... Know deep how each of the Methods work independently and together is where the challenge it. 
+
 const companies = [
     {name: "Company One", category: "Finance", start: 1981, end: 2003},
     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
@@ -12,6 +14,7 @@ const companies = [
     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
   ];
+  let ages = [33, 12, 20, 16,100, 5, 54, 21, 44, 61,31,15,45,25,64,32];
 
  //onsole.log(companies)
 
@@ -33,9 +36,8 @@ const companies = [
 //      */
 // })
       
-let age = [33, 12, 20, 16,100, 5, 54, 21, 44, 61];
 
-    // --------filter----- GET 21 AN OLDER
+// --------filter----- GET 21 AN OLDER
 
 //      // Using ForLoop
 // let canDrink = []
@@ -56,20 +58,175 @@ let age = [33, 12, 20, 16,100, 5, 54, 21, 44, 61];
 
   // Filter out companies that are Retails. 
 
-const retailComp = companies.filter(function (company) {
-  // console.log(company.category)
-   if(company.category === "Retail") return company
-})
-   console.log(retailComp)
+// let retailComp = companies.filter(function (company) {
+//   // console.log(company.category)
+//    if(company.category === "Retail") return company
+// })
+    // -----------OR----------
+   // const retailComp = companies.filter(company => company.category === "Retail")
 
-//............ End of Filter........
 
-  //map
-  //sort
-  //reduce
-  //for..in
-  //spread
+   //console.log(retailComp)
+
+    /*
+     { name: 'Company Two', 
+    category: 'Retail', 
+    start: 1992, 
+    end: 2008 }, 
+  { name: 'Company Four', 
+    category: 'Retail', 
+    start: 1989, 
+    end: 2010 }, 
+  { name: 'Company Nine', 
+    category: 'Retail', 
+    start: 1981, 
+    end: 1989 } ] 
+  at ​​​retailComp​​​ ​java
+    */
+   
+
+     //--------------------Get companies that Start in the 80s
+/// Note: filter can take an CALLBACK, INDEX,ARRAY
+/*
+const eightyComp = companies.filter(function (company, index, array) {
+  if (company.start >= 1980 && company.start <= 1990) { return company }
+  console.log(index)  // => 1,2,4,7
+ }
+)
+
+console.log(eightyComp)
+*/
+
+// Get Company Last more than 10 years. 
+  /*
+let companiesLastTenYr = companies.filter(company => company.end - company.start >= 10)  
   
 
-  //splice..
+
+console.log(companiesLastTenYr)
+   */
+//............................. End of Filter............
+
+ 
+// -------------------Map------------------------------
+  //map   .... Map can take in a Index aswell.  
+  //const ageSquare = ages.map(function (age, index){ return Math.sqrt(age)}) 
+
+
+  /*
+   can do something like.... 
+    const ageMap = ages
+        .map(age => math.sqrt(age))    => we square it here.
+        .map(age =>age * 2)  => mutiply by 2. 
+  
+  */
+
+
+
+const companyNames = companies.map(function (company) {
+
+   // get company name, Start and end Date.
+  return `${company.name} [${company.start}]   [${company.end}] `
+     //return List of Company  == >  [ 'Company One [1981]   [2003] ',
+    }   
+)
+// console.log(companyNames )
+
+
+
+// const ageSquare = ages.map(function (age, index){ return Math.sqrt(age)})
+ 
+// console.log(ageSquare) 
+
+//    /*
+//    5.744562646538029, 
+//   3.4641016151377544, 
+//   4.47213595499958, 
+//   4, 
+//   10, 
+//   2.23606797749979, 
+//   7.3484692283495345, 
+//   4.58257569495584, 
+//   6.6332495807108, 
+//   7.810249675906654 ] 
+   
+//    */
+
+
+  //----------------____End  of Map------------------
+
+
+
+
+  //----------------sort-----------Sort company by start year - end year----------------
+
+const sortedCompanies = companies.sort(function (c1, c2) {
+     //Note: c1 - company1 and company 2 ///  Sorting by Start date of company. 
+   return (c1.start > c2.start) ? 1 : -1
+  })
+   
+
+  //console.log(sortedCompanies)
+   
+let orderAge = ages.sort(function (a, b) {
+   return (b - a  ) //=>[ 100, 61, 54, 44, 33, 21, 20, 16, 12, 5 ] 
+   // return a -b,  goes from smallest to largets. 
+ 
+  })
+  //console.log(orderAge)
+
+  //-------------------End of SORT----------------
+
+
+  //-------------reduce-----------------------
+     // Look to add all the numbers into 1.
+     /*
+     arr.reduce(callback( accumulator, currentValue[, index[, array]] ) {
+  // return result from executing something for accumulator or currentValue
+}[, initialValue]);
+      
+      */
+     
+// let ageSum = 0
+
+//       for (let i = 0; i < ages.length; i++) {
+//         ageSum += ages[i];  //=> 366
+        
+//       }
+//       console.log(ageSum)  => 366
+
+
+const reduceAge = ages.reduce(function (total, age) {
+   // Can take an index too
+  return total + age
+
+},  0)
+  
+  //console.log(reduceAge)
+
+  // Get Total Years for Each companys. 
+const totalYears = companies.reduce(function (accumulator, company) {
+   return accumulator += company.end - company.start
+   
+ },0)
+
+
+// console.log(totalYears) => 118
+  //------------end of reduce---------------
+
+
+
+
+
+  //for..in
+  //spread
+  //slice..
   // ..
+
+
+  //------------ Combined Methods----------------------
+
+
+
+
+  // -------- End of Combined Methods--------------------
