3a85acef41ce8a27b250ef3aa10886ff616eb519
EPIC448 <samrey2018@gmail.com>
2021-05-14 21:10:42 -0400

    Code interviews updated

--- comon-sense-guide -to-DSA/code_interviews/nextCapital_Interview.js
+++ comon-sense-guide -to-DSA/code_interviews/nextCapital_Interview.js
@@ -24,7 +24,7 @@ function solution(message, K) {
 // let k =  14
 // console.log(solution(teststr, k))
 
-
+// ========== Next Problem======
 
 //  Given an array 1s and 0s, find the longest sequence 
 // of 1s and return return its starting INDEX
@@ -52,37 +52,37 @@ function solution(message, K) {
 
 
 
-// function solution(A) {
-//     // A = array
-//     // n= array.length
-//     var n = A.length;
-//     //index => A[9] = 1
-//     var i = n - 1;
-//     var result = -1;
-//     //max = numberofoccurance, k= kount
-//     var maximal = 0, k = 0;
-//     while (i > 0) {
-//         if (A[i] == 1) {
-//             k = k + 1;
-//             if (k >= maximal) {
-//                 maximal = k;
-//                 result = i;
-//             }
-//         }
-//         else
-//             k = 0;
-//         i = i - 1; 
-//     }
-//     if (A[i] == 1 && k + 1 > maximal)
-//         result = 0;
-//     return result;
-// }
+function solution(A) {
+    // A = array
+    // n= array.length
+    var n = A.length;
+    //index => A[9] = 1
+    var i = n - 1;
+    var result = -1;
+    //max = numberofoccurance, k= kount
+    var maximal = 0, k = 0;
+    while (i > 0) {
+        if (A[i] == 1) {
+            k = k + 1;
+            if (k >= maximal) {
+                maximal = k;
+                result = i;
+            }
+        }
+        else
+            k = 0;
+        i = i - 1; 
+    }
+    if (A[i] == 1 && k + 1 > maximal)
+        result = 0;
+    return result;
+}
 
-// let test1 =  [1, 0, 1, 1, 0, 1, 1, 1, 0, 1]
+let test1 =  [1, 0, 1, 1, 0, 1, 1, 1, 0, 1]
 
-// let test2 = [0,0]
+let test2 = [0,0]
 
-// let test3 = [0, 1, 1, 1, 0, 1, 1, 1, 1, 1]
+let test3 = [0, 1, 1, 1, 0, 1, 1, 1, 1, 1]
 
 
-// console.log(solution(test3))
+console.log(solution(test3))
