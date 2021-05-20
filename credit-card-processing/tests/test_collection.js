1808a24f72d40e34f0b1d04918c0c43296eacdee
EPIC448 <samrey2018@gmail.com>
2021-05-19 20:00:14 -0400

    completed test_luhn.js file

---
+++ credit-card-processing/tests/test_collection.js
@@ -0,0 +1,7 @@
+// import {default as namedtuple, deque } from '@aureooms/js-collections' ;
+
+// import {default as luhnObj} from '../utils/luhn.js'
+
+// console.log(luhnObj._luhn_checksum)
+// // console.log(_luhn_checksum)
+// // console.log(is_luhn_valid)
